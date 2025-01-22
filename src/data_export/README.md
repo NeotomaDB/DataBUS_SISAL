ea824b95d3d2c1d57a40d4f023f4b19fe2268f01
Simon <simon.j.goring@gmail.com>
2025-01-22 14:10:57 -0800

    Updated the export script with assertions, added README.
    I also added all `csv#` files to the gitignore, so opening up the csv file with a spreadsheet editor isn't going to cause new files to be committed.

---
+++ src/data_export/README.md
@@ -0,0 +1,27 @@
+# Export Data From SISAL
+
+This folder is intended to be used to export data directly from the [SISAL database](https://pastglobalchanges.org/science/wg/sisal/intro) into a set of comma separated data files, one for each individual site (or composite site) within the database for use in the more comprehensive DataBUS workflow.
+
+The script connects to a local instance of the SISAL MySQL database using a connection string read from a `.env` file in this directory. The `.env` file is not included in this repository because it is likely to include usernames and passwords that should not be shared publically.
+
+The file should have the structure:
+
+```env
+SISAL_CONNECT={"host":"127.0.0.1","port":3306,"user":"USERNAME","password":"PASSWORD","database":"sisal"}
+```
+
+The scrpt itself reads in this environment variable using the python package `python-dotenv`.
+
+## Expected Behaviour
+
+To run the script, type:
+
+`uv run export_by_site.py`
+
+The script will then open the database connection, read in all the unique `site_id` values in the database, and return a single file for each site into a `./data` folder in this repository. Each `csv` file should have a set of unique columns that match columns in the `sisal_template.csv` or `sisal_template.yaml` file.
+
+## Modifying the File Output
+
+The file output is determined by the [wide_export](./sql/wide_export.sql) file which performs a set of `LEFT JOIN`s on the tables in SISAL to generate a comprehensive output for each record. The query as currently written dumps all contents of each table using `SELECT table.*`. We then strip down the full output within the function `write_to_csv()`. This is a bit lazy and may cause complications if multiple tables have variables of the same name with different meanings (see for example the column 'notes' in Neotoma).  To modify file outputs, the first option should be modifying [wide_export](./sql/wide_export.sql).
+
+The file output location is `../../data` relative to this directory location. To change this, look at the `assert` statements in the `export_by_site.py` file, and in the `write_to_csv()` function.
--- src/data_export/export_by_site.py
+++ src/data_export/export_by_site.py
@@ -1,10 +1,23 @@
-import mysql.connector
+from mysql.connector import connect
 from dotenv import load_dotenv
 from json import loads
 from os import getenv
+from os import path
 import csv
 
-def write_to_csv(sisal:list, colnames:tuple):
+
+def write_to_csv(sisal:list, colnames:tuple) -> None:
+    """_Write the SISAL Query output for a single site to file._
+
+    Args:
+        sisal (list): _A list of tuples returned from the query cursor._
+        colnames (tuple): _A tuple of colnames that are linked to the sisal list._
+
+    Returns:
+        _None_: _This function writes to file, and does not return anything._
+    """
+    assert all(len(x) == len(sisal[0]) for x in sisal), "All list elements must be the same length."
+    assert len(colnames) == len(sisal[0]), "The number of column names must be the same as the list element lengths."
     keys = colnames
     tofile = []
     for i in sisal:
@@ -14,33 +27,36 @@ def write_to_csv(sisal:list, colnames:tuple):
                 outdict[keys[k]] = i[k] 
         tofile.append(outdict)
     new_keys = outdict.keys()
-    with open(f'../../data/sisal_site_{sisal[0][0]}.csv', 'w', newline='') as output_file:
+    with open(f'../../data/sisal_site_{sisal[0][0]}.csv', 'w', newline='', encoding="UTF-8") as output_file:
         dict_writer = csv.DictWriter(output_file, new_keys)
         dict_writer.writeheader()
         dict_writer.writerows(tofile)
     return None
 
+assert path.isdir('../../data'), "There is no directory at '../../data'."
 
 call_siteids = """
-SELECT DISTINCT site.site_id
-FROM site"""
+    SELECT DISTINCT site_id
+    FROM site
+"""
 
 load_dotenv()
 CONN_STRING = loads(getenv("SISAL_CONNECT"))
         
-con = mysql.connector.connect(**CONN_STRING)
+con = connect(**CONN_STRING)
 
 cur = con.cursor()
 cur.execute(call_siteids)
 row = cur.fetchall()
+
+assert len(row) > 0, "No sites were returned. Is the database present and intact?"
 
 # Load in the larger query:
 with open('sql/wide_export.sql') as f:
     big_query = f.read()
 
 for i in row:
-    cur.execute(big_query, i)
+    cur.execute(big_query, {'site_id': i[0]})
     colnames = cur.column_names
     site_output = cur.fetchall()
     write_to_csv(site_output, colnames)
-
